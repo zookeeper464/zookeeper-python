c17255555ed1b296d55fe0f5f36b00b79a34718d
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-10 23:00:51 +0900

    Create silver3_11726.py
    2021.02.10

---
+++ silver3_11726.py
@@ -0,0 +1,15 @@
+N=int(input())
+input_list=[]
+
+result_list=[0]*(N+1)
+result_list[1]=1
+try:
+  result_list[2]=2
+except:
+  pass
+  
+
+for i in range(3,N+1):
+  result_list[i]=result_list[i-1]+result_list[i-2]
+
+print(result_list[N])
