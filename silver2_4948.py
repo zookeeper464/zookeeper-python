547cea3d21bd050ffc8b49ff7bed8b81747f158f
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-15 11:06:04 +0900

    베르트랑 공준 수정 ""
    2021.02.15

--- silver2_4948.py
+++ silver2_4948.py
@@ -21,3 +21,36 @@ for i in val_list:
   checker(i)
 
 print(count)
+
+"""
+def checker (n):
+  global p_list
+  for i in range(2*n+1):
+    temp=0
+    for j in range(2,i):
+      if i%j==0:
+        temp+=1
+    if temp==0:
+      p_list.append(i)
+      
+p_list=[]
+val_list=[]
+T=1
+while T>0 and T<=123456:
+  T=int(input())
+  if T==0:
+    max_val=max(val_list)
+    break
+  else:
+    val_list.append(T)
+checker(max_val)
+
+
+for i in val_list:
+  count=0
+  for j in p_list:
+    if j>i and j<=2*i:
+      count+=1
+  print(count)
+
+"""
