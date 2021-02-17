2dedb937669d483f60574ea92745c19ec5cf0d77
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-18 07:08:03 +0900

    크로아티아 알파벳
    2021.02.18수정

--- silver5_2941.py
+++ silver5_2941.py
@@ -1,14 +1,16 @@
 def checker(char):
     length=len(char)
-    
+
     for i in range(length):
         if "dz=" in char:
             length-=1
         for i in ["c=","c-","d-","lj","nj","s=","z="]:
             if i in char:
-                char=char.replace(i,"",1)
-                length-=1
+                n=char.count(i)
+                char=char.replace(i," ")
+                length-=n
+                print(char)
     print(length)
 
 char=input()
-checker(char)
+checker(char) 
