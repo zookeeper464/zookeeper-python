15d330ea6db67b0191afa727a9ee4c39bc96c140
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-19 16:22:52 +0900

    수 정렬하기 2
    2021.02.19

--- silver5_2751.py
+++ silver5_2751.py
@@ -1,9 +1,36 @@
+def heapify(unsorted, index, heap_size):
+  largest=index
+  l_index=2*index+1
+  r_index=2*index+2
+  
+  if l_index<heap_size and unsorted[l_index]>unsorted[largest]:
+    largest=l_index
+
+  if r_index<heap_size and unsorted[r_index]>unsorted[largest]:
+    largest=r_index
+  
+  
+  if largest!=index:
+    unsorted[largest], unsorted[index] = unsorted[index], unsorted[largest]
+    heapify(unsorted, largest, heap_size)
+
+def heap_sort(unsorted):
+  n=len(unsorted)
+  for i in range(n//2-1, -1,-1):
+    heapify(unsorted,i,n)
+    
+  for i in range(n-1,0,-1):
+    unsorted[0],unsorted[i]=unsorted[i],unsorted[0]
+    heapify(unsorted,0,i)
+    
+  return unsorted
+
 N=int(input())
-temp_list=[]
+val_list=[]
 for i in range(N):
-    a=int(input())
-    if a not in temp_list:
-        temp_list.append(a)
-new_list=sorted(temp_list)
-for i in new_list:
-    print(i)
+  a=int(input())
+  val_list.append(a)
+
+lst=heap_sort(val_list)
+for i in lst:
+  print(i)
