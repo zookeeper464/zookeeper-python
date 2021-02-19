2de487fd4fee5ae1be0a42fa69e8b3a5db74c692
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-19 17:00:17 +0900

    최대공약수와 최소공배수
    2021.02.19

--- silver5_2609.py
+++ silver5_2609.py
@@ -1,11 +1,8 @@
-a,b=map(int, input().split())
-
-val=max(a,b)
-min_val=0
-max_val=0
-for i in range(1,val):
+a, b=map(int,input().split())
+c=min(a,b)
+for i in range(1,c+1):
   if a%i==0 and b%i==0:
-    min_val=i
-max_val=int(a*b/min_val)
-print(min_val)
-print(max_val)
+    d=i
+
+print(d)
+print((a*b)//d)
