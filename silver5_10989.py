79a7792c8be63efe53720322f0dc3577f3a525ed
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-20 16:20:24 +0900

    수 정렬하기3
    2021.02.20

--- silver5_10989.py
+++ silver5_10989.py
@@ -1,9 +1,8 @@
-N=int(input())
-val_list=[]
-
-for i in range(N):
-  a=int(input())
-  val_list.append(a)
-val_list.sort()
-for i in range(N):
-  print(val_list[i])
+n = int(input())
+cnt = [0 for i in range(10000+1)]
+for i in range(n):
+  cnt[int(input())] += 1
+for i in range(1, 10000+1):
+  if cnt[i]!=0:
+    for j in range(cnt[i]):
+      print(i)
