c9cdb39397c1dc8025303435b370321ecbdb88a3
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-23 08:12:43 +0900

    단어 정렬
    2021.02.23

--- silver5_1181.py
+++ silver5_1181.py
@@ -1,11 +1,19 @@
 N=int(input())
 str_list=[]
+for i in range(50):
+  str_list.append([])
+
 for i in range(N):
   a=input()
-  str_list.append(a)
+  n=len(a)
+  if a not in str_list[n-1]:
+    str_list[n-1].append(a)
 
-str_list.sort()
-str_list.sort(key=lambda x:len(x))
+for i in range(50):
+  if str_list[i]:
+    str_list[i].sort()
 
-for i in str_list:
-  print(i)
+for i in range(50):
+  if str_list[i]:
+    for j in str_list[i]:
+      print(j)
