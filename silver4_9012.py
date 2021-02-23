1e3e7a3cf84ce1bb0cec54c45ea7da662082228e
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-23 15:41:34 +0900

    괄호
    2021.02.23

--- silver4_9012.py
+++ silver4_9012.py
@@ -6,9 +6,15 @@ for i in range(N):
   val_list.append(a)
 
 for i in val_list:
-  temp1=i.count(')')
-  temp2=i.count('(')
-  if temp1==temp2:
+  count=0
+  for j in i:
+    if j=="(":
+      count+=1
+    elif j==")":
+      count-=1
+    if count<0:
+      break
+  if count==0:
     print("YES")
   else:
     print("NO")
