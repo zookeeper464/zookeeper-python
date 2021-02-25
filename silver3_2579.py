8567271dcd57d191283aadf7176f37eb03f05239
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-25 16:23:51 +0900

    계단 오르기
    2021.02.25
    틀림. 시간 줄여야함

--- silver3_2579.py
+++ silver3_2579.py
@@ -4,24 +4,15 @@ for i in range(N):
   a=int(input())
   input_list.append(a)
 
-N_list=[[],[[0,1]],[[0,1,1]]]
-if N>2:
-  for i in range(3,N+1):
-    temp_list=[]
-    a,b=N_list[i-2],N_list[i-3]
-    for j in a:
-      temp=j+[0,1]
-      temp_list.append(temp)
-    for j in b:
-      temp=j+[0,1,1]
-      temp_list.append(temp)
-    N_list.append(temp_list)
+def stair (n):
+  global input_list
+  if n==1 or n==0:
+    return input_list[n]
+  elif n==2:
+    a=input_list[1]+input_list[2]
+    return a
+  else:
+    a=max(input_list[n]+stair(n-2), input_list[n]+input_list[n-1]+stair(n-3))
+    return a
 
-result_list=[]
-for i in N_list[N]:
-  result=0
-  for j in range(N+1):
-    result+=i[j]*input_list[j]
-  result_list.append(result)
-
-print(max(result_list))
+print(stair(N))
