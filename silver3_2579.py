7f40e9f06ad4b5ec1309de2f2d18977dd0dbbceb
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-26 10:36:18 +0900

    계단오르기
    2021.02.26

--- silver3_2579.py
+++ silver3_2579.py
@@ -1,18 +1,13 @@
-N=int(input())
-input_list=[0]
-for i in range(N):
-  a=int(input())
-  input_list.append(a)
+N = int(input())
+arr = [0]
+memo = [0] * (N+1)
+for _ in range(0, N) :
+    arr.append(int(input()))
 
-def stair (n):
-  global input_list
-  if n==1 or n==0:
-    return input_list[n]
-  elif n==2:
-    a=input_list[1]+input_list[2]
-    return a
-  else:
-    a=max(input_list[n]+stair(n-2), input_list[n]+input_list[n-1]+stair(n-3))
-    return a
-
-print(stair(N))
+memo[1] = arr[1]
+if N>=2:
+  memo[2] = memo[1] + arr[2]
+if N>=3:
+  for i in range(3, len(memo)):
+      memo[i] = max(arr[i] + memo[i-2] , arr[i] + memo[i-3] + arr[i-1])
+print(memo[N])
