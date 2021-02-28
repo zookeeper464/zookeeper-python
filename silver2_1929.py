fc5821c704d30f8fd98d33c4faed3a3ed35a9508
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-02-28 20:35:12 +0900

    소수 구하기
    2021.02.28

--- silver2_1929.py
+++ silver2_1929.py
@@ -1,12 +1,10 @@
-N,M=map(int,input().split())
-p_list=list(range(N,M+1))
-temp_list=[]
-
-
-for i in p_list:
+M,N=map(int,input().split())
+from math import sqrt
+for i in range(M,N+1):
   count=0
-  for j in range(1,i):
+  for j in range(2,int(sqrt(i))+1):
     if i%j==0:
-      count+=1
-  if count==1:
+      count=1
+      break
+  if count==0 and i>1:
     print(i)
