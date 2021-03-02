f6a4ed9f08b7fbd652e7e570ede32991ebbe0518
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-03-02 21:02:32 +0900

    하노이 탑 이동 순서
    2021.03.02

--- silver2_11729.py
+++ silver2_11729.py
@@ -1,16 +1,17 @@
-K=int(input())
-print(2**K-1)
-
-def change (T, N, M):
-  if T==1:
-    print(f"{N} {M}")
+N=int(input())
+print(2**N-1)
+lst=[]
+def move(num,start,last,mid,lst):
+  if num==1:
+    lst.append([start,last])
   else:
-    a=[1,2,3]
-    a.remove(N)
-    a.remove(M)
-    temp=a[0]
-    change(T-1, N, temp)
-    print(f"{N} {M}")
-    change(T-1, temp, M)
+    move(num-1,start,mid,last,lst)
+    lst.append([start,last])
+    move(num-1,mid,last,start,lst)
 
-change(K, 1, 3)
+move(N,1,3,2,lst)
+
+for i in lst:
+  for j in range(2):
+    print(i[j],end=" ")
+  print()
