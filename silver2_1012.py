4afcebfda1978705c4668c95c6d192c0f71e6006
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-03-02 13:28:58 +0900

    유기농 배추
    2021.03.02

--- silver2_1012.py
+++ silver2_1012.py
@@ -1,7 +1,32 @@
-T=int(input())
-for i in range(T):
-  N,M,K=map(int,input().split())
-  for i in range(K):
-    a,b=map(int,input().split())
+def checker(lst,a,b):
+  lst[a][b]=False
+  dx=[-1,1,0,0]
+  dy=[0,0,-1,1]
+  for i in range(4):
+    if lst[a+dx[i]][b+dy[i]]:
+      checker(lst,a+dx[i],b+dy[i])
 
-#알고리즘을 모르겠다.
+
+T=int(input())
+answer=[]
+for i in range(T):
+  M,N,K=map(int,input().split())
+
+  lst=[]
+  for j in range(N+2):
+    lst.append([False]*(M+2))
+
+  li=[]
+  for j in range(K):
+    b,a=map(int,input().split())
+    lst[a+1][b+1]= True
+    li.append([a+1,b+1])
+  
+  count=0
+  for j in li:
+    if lst[j[0]][j[1]]:
+      count+=1
+      checker(lst,j[0],j[1])
+  answer.append(count)
+for i in answer:
+  print(i)
