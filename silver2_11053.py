98ef02d082f5aef6c4948c5109021c6fcd42cb45
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-03-02 14:17:37 +0900

    가장 긴 증가하는 부분 수열
    2021.03.02

--- silver2_11053.py
+++ silver2_11053.py
@@ -1,3 +1,20 @@
 N=int(input())
-input_list=list(map(int,input().split()))
-print(len(set(input_list)))
+lst=list(map(int,input().split()))
+sol=[]
+for i in range(N):
+  sol.append(0)
+
+def checker(sol,lst,i,N):
+  if i==N-1:
+    sol[i]=1
+  else:
+    temp=[0]
+    for j in range(i+1,N):
+      if lst[j]>lst[i]:
+        temp.append(sol[j])
+    sol[i]=max(temp)+1
+
+for i in range(N-1,-1,-1):
+  checker(sol,lst,i,N)
+
+print(max(sol))
