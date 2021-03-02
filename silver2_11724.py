3baca74ba0a1858b588e74594802225ee7c508b6
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-03-02 20:47:00 +0900

    연결 요소의 개수
    2021.03.02

--- silver2_11724.py
+++ silver2_11724.py
@@ -1,27 +1,24 @@
-count=0
+def DFS (i,visited,e_list):
+  visited[i]=True
+  for j in e_list[i]:
+    if not visited[j]:
+      DFS(j, visited, e_list)
+
 N,M=map(int,input().split())
-val_list=[]
+e_list=[[]]
+visited=[True]
+count=0
+for i in range(N):
+  e_list.append([])
+  visited.append(False)
 for i in range(M):
   a,b=map(int,input().split())
-  if a>b:
-    b,a=a,b
-  val_list.append([a,b])
-val_list.sort()
+  e_list[a].append(b)
+  e_list[b].append(a)
 
-temp_list=[]
-for i in range(1,N+1):#1~N까지
-  if i in temp_list:#temp_list에 없을때만 사용하는 것
-    pass
-  else:#temp_list에 없으면 실행
-    temp_list.append(i)#이제 추가한다.
-    temp_set={i}#i에 대한 새로운 집합생성
-    for j in val_list:
-      set_j=set(j)
-      if temp_set&set_j==set():
-        pass
-      else:
-        temp_set=temp_set|set_j
+for i in range(1,N+1):
+  if not visited[i]:
+    DFS(i,visited,e_list)
     count+=1
-    temp_list=list(set(temp_list)|temp_set)
-    print(temp_list)
+    
 print(count)
