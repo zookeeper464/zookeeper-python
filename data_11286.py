b4cb8f2ffdbda0761f112ddb292b797b07d9b3d4
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-04-04 23:12:26 +0900

    절댓값 힙
    2021.04.04

--- data_11286.py
+++ data_11286.py
@@ -5,40 +5,19 @@ input = sys.stdin.readline
 n = int(input())
 h = []
 answer = []
-dic = {}
-#기본 입력 변수 입력
-
 for i in range(n):
   m = int(input())
   if m == 0:
     if h:
       temp = heapq.heappop(h)
-      if dic[abs(temp)] == 0:
-        answer.append(temp)
-      else:
-        answer.append(temp * (-1))
-        dic[abs(temp)] -= 1
-    #m이 0이고 h가 null이 아닐때, 출력 메커니즘
-
+      answer.append(temp[0] * ((-1)**temp[1]))
     else:
       answer.append(0)
-    #m이 0이고 h가 null일때, 출력 메커니즘
-
   elif m > 0:
-    heapq.heappush(h,m)
-    try:
-      dic[abs(m)] += 0
-    except:
-      dic[abs(m)] = 0
-    #양수일때 입력 메커니즘
-
+    heapq.heappush(h,(m,2))
   else:
-    heapq.heappush(h,abs(m))
-    try:
-      dic[abs(m)] += 1
-    except:
-      dic[abs(m)] = 1
-    #음수일때 입력 메커니즘
+    heapq.heappush(h,(abs(m),1))
+
 
 for i in answer:
   print(i)
