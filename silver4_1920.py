5ec62019281d4f608b5660a8a36af73d2b8190d2
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-04-11 17:48:32 +0900

    Update silver4_1920.py

--- silver4_1920.py
+++ silver4_1920.py
@@ -1,15 +1,2 @@
-N=int(input())
-location_list=[]
-
-temp=input().split()[:N]
-N_list=list(map(int,temp))
-
-M=int(input())
-temp=input().split()[:M]
-M_list=map(int,temp)
-
-for i in M_list:
-  if i in N_list:
-    print(1)
-  else:
-    print(0)
+#다른 파일 참고
+#집합을 이용 또는 이분 탐색 이용하면 
