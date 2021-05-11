27b6b25c4151f111ed66caa47cb23ab10810b33f
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-11 16:35:32 +0900

    상근날드

---
+++ 코딩테스트/수학/5543.py
@@ -0,0 +1,10 @@
+bugers = []
+drinks = []
+
+for i in range(3):
+  bugers.append(int(input())
+                
+for i in range(2):
+  drinks.append((int(input())
+                 
+print(min(bugers)+min(drinks)-50)
