93eef75c270ea6a8c89fe49a34ee623d871b1b18
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-18 23:05:39 +0900

    분수 합

--- 코딩테스트/수학/1735.py
+++ 코딩테스트/수학/1735.py
@@ -2,5 +2,5 @@ from math import gcd
 a, b = map(int,input().split())
 x, y = map(int,input().split())
 
-g = gcd(b,y)
-print((a*y//g)+(x*b//g),b*y//g)
+g = gcd(a*y+x*b,b*y)
+print((a*y+x*b)//g,b*y//g)
