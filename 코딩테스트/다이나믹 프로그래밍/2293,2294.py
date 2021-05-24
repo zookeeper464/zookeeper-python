724d9eacaec3d0cb1f343f4d02b2a1b354311fc7
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-24 11:22:34 +0900

    동전,동전2

--- 코딩테스트/다이나믹 프로그래밍/2293,2294.py
+++ 코딩테스트/다이나믹 프로그래밍/2293,2294.py
@@ -1,11 +1,13 @@
 n, k = map(int,input().split())
 
-dp = [0 for _ in range(k+1)]
+dp = [-1 for _ in range(k+1)]
 dp[0] = 1
 for i in range(n):
   a=int(input())
   for i in range(k+1-a):
-    if dp[i]!=0:
+    if dp[i]!=-1:
+      if dp[a+i] == -1:
+        dp[a+i] = 0
       dp[a+i]+=dp[i]
 
 
