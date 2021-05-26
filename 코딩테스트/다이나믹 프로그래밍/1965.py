92dcbafe2db83b44fead03e8b1ff91e43983697c
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-26 22:28:00 +0900

    상자넣기

---
+++ 코딩테스트/다이나믹 프로그래밍/1965.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int,input().split()))
+dp = [1 for _ in range(n)]
+for i in range(1,n):
+  for j in range(i):
+    if lst[i]>lst[j] and dp[i]<dp[j]+1:
+      dp[i] = dp[j]+1
+
+print(max(dp))
