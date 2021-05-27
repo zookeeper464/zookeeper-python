2c698ea9bcc98c24ccbb4ebd9d5843a49aeffc96
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-27 17:55:47 +0900

    시험 성적

---
+++ 코딩테스트/구현/9498.py
@@ -0,0 +1,12 @@
+90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
+n = int(input())
+if 90<=n<=100:
+  print("A")
+elif 80<=n<90:
+  print("B")
+elif 70<=n<80:
+  print("C")
+elif 60<=n<70:
+  print("D")
+else:
+  print("F")
