71e6ec0ea7b0b99b813ff8162e5e11cb77c55849
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-27 17:28:21 +0900

    구간 합 구하기 5
    항상 dp를 다룰 때 이게 필요한데 어떻게 줄여야지? 이게 아니라 일단 만들어야 한다.

---
+++ 코딩테스트/다이나믹 프로그래밍/11660.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+dp = [[0]*(n+1)]
+for i in range(n):
+  s = list(map(int,input().split()))
+  for j in range(1,n):
+    s[j] += s[j-1]
+  s=[0]+s
+  dp.append(s)
+for i in range(2, n+1):
+  for j in range(1, n+1):
+    dp[i][j]+=dp[i-1][j]
+
+
+answer = []
+for i in range(m):
+  a, b, c, d = map(int,input().split())
+  temp = dp[c][d]+dp[a-1][b-1]-dp[c][b-1]-dp[a-1][d]
+  answer.append(temp)
+
+for i in answer:
+  print(i)
