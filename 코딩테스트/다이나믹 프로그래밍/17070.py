3e3c26e2a2a8684f06f32f55807ac0ab404cea99
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-27 14:19:39 +0900

    파이프 옮기기 1
    종류가 여러개 존재할 경우 dp도 여러개 만들면 된다.
    출발지점과 도착지점의 반례는 항상 생각하자

---
+++ 코딩테스트/다이나믹 프로그래밍/17070.py
@@ -0,0 +1,31 @@
+n = int(input())
+lst1 = [list(map(int,input().split())) for _ in range(n)]
+for i in range(n):
+  for j in range(n):
+    if lst1[i][j] == 1:
+      lst1[i][j] = -1
+
+lst2, lst3 = [], []
+for i in range(n):
+  lst2.append(lst1[i][:])
+  lst3.append(lst1[i][:])
+
+for i in range(1,n):
+  if lst1[0][i] != -1:
+    lst1[0][i] = 1
+  else:
+    break
+if i==1 or lst1[-1][-1] == -1:
+  print(0)
+else:
+  for i in range(1,n):
+    for j in range(1,n):
+      if lst1[i][j]>-1:
+        if lst1[i][j-1]>-1:
+          lst1[i][j] += lst1[i][j-1]+lst2[i][j-1]
+        if lst3[i-1][j]>-1:
+          lst3[i][j] += lst3[i-1][j]+lst2[i-1][j]
+        if lst1[i-1][j-1]>-1 and lst1[i][j-1]>-1 and lst1[i-1][j]>-1:
+          lst2[i][j] += lst1[i-1][j-1]+lst2[i-1][j-1]+lst3[i-1][j-1]
+
+  print(lst1[-1][-1]+lst2[-1][-1]+lst3[-1][-1])
