2ba9928421a43bd2c02817cf18f862c0dc1fdbe4
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-05-28 09:58:16 +0900

    문자열 반복

---
+++ 코딩테스트/구현/2675.py
@@ -0,0 +1,12 @@
+n = int(input())
+answer = []
+for _ in range(n):
+  m, s = input().split()
+  m = int(m)
+  temp = ""
+  for i in s:
+    temp += i*m
+  answer.append(temp)
+
+for i in answer:
+  print(i)
