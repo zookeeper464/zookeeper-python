28bfdae277f6dcbb0e61e9258184b511110c2013
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-06-04 09:09:06 +0900

    2차원 배열의 합

---
+++ 코딩테스트/구현/2167.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+dp = [[0] * (m+1)]+[[0]+list(map(int,input().split())) for _ in range(n)]
+
+for i in range(1,n+1):
+  for j in range(1,m+1):
+    dp[i][j] += dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]
+
+answer = []
+k = int(input())
+for _ in range(k):
+  i,j,x,y = map(int,input().split())
+  answer.append(dp[x][y]-dp[x][j-1]-dp[i-1][y]+dp[i-1][j-1])
+
+for i in answer:
+  print(i)
