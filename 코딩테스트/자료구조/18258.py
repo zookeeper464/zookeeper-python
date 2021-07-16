95fabc41756e03bb9f2b50c7c9de208abd2185ae
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-07-16 19:08:28 +0900

    큐 2
    주어진 조건에 맞춰진 함수를 생성하여 값을 출력한다.

---
+++ 코딩테스트/자료구조/18258.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n = int(input())
+q= deque()
+answer = []
+
+def command (s):
+  if len(s.split()) == 1:
+    if s[0] == "e":
+      if q:
+        return 0
+      else:
+        return 1
+    
+    elif s[0] == "s":
+      return len(q)
+    
+    if not q:
+      return -1
+
+    if s[0] == "p":
+      num = q.popleft()
+      return num
+    elif s[0] == "f":
+      return q[0]
+    else:
+      return q[-1]
+  
+  else:
+    num = s.split()[1]
+    q.append(num)
+    return "push"
+
+for _ in range(n):
+  s = input()
+  temp = command(s)
+  if temp != "push":
+    answer.append(temp)
+
+for ans in answer:
+  print(ans)
