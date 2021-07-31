be320c4fa12bd15745d4adcd5511ac9dd04df240
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-08-01 00:21:05 +0900

    보석 도둑
    데이터를 저장하고 이를 정렬한 뒤 탐색하는 과정을 거쳤다.
    완전 탐색을 실시하였으므로 상당히 오랜 시간이 걸린다.
    좀 더 빠르게 탐색하는 방법을 찾아야 한다.

--- 코딩테스트/그리디 알고리즘/1202.py
+++ 코딩테스트/그리디 알고리즘/1202.py
@@ -1,1 +1,35 @@
+n,k = map(int,input().split())
+dia_dict = dict()
 
+for _ in range(n):
+  a,b = map(int,input().split())
+  dia_dict[a] = b
+
+bag_list = []
+for _ in range(k):
+  m = int(input())
+  bag_list.append(m)
+
+key_list = list(dia_dict.keys())
+bag_list.sort(reverse=True)
+key_list.sort()
+dia = [True] * n
+answer = 0
+
+for _ in range(k):
+  temp = bag_list.pop()
+  val = 0
+  idx = -1
+  for i in range(n):
+    if temp>= key_list[i]:
+      if dia[i] and val < dia_dict[key_list[i]]:
+        val = dia_dict[key_list[i]]
+        idx = i
+        dia[i] = False
+    else:
+      break
+  if idx >-1:
+    dia[idx] = False
+    answer += val
+
+print(answer)
