9427f574c9d6ced21fee0890b3b5ce72c53777cd
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-08-04 17:40:32 +0900

    토마토
    데이터를 탐색하는 과정을 동일하게 사용했다.
    메모리 사용 용량을 활용하는데 함수를 사용하여 함수 속으로 계속 들어간다면 메모리가 너무 많이 사용되어 용량을 넘기게 된다.
    그렇기 때문에 함수가 아닌 while문을 통해서 이를 활용하도록 한다.

---
+++ 코딩테스트/그래프 탐색/7576.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+n, m = map(int,input().split())
+lst = [list(map(int,input().split())) for _ in range(m)]
+q = deque()
+dr, dc = [1,0,-1,0],[0,1,0,-1] 
+
+for r in range(m):
+  for c in range(n):
+    if lst[r][c] == 1:
+      q.append([r,c])
+    elif lst[r][c] == -1:
+      lst[r][c] = 1
+      
+t = 0
+while True:
+  l = len(q)
+  for _ in range(l):
+    r,c = q.popleft()
+    for i in range(4):
+      nr,nc = r+dr[i],c+dc[i]
+      if 0<=nr<m and 0<=nc<n and lst[nr][nc] == 0:
+        q.append([nr,nc])
+        lst[nr][nc] = 1
+  if q:
+    t += 1
+    break
+
+s = 0
+for r in range(m):
+  s += sum(lst[r])
+
+if s == n*m:
+  print(t)
+else:
+  print(-1)
