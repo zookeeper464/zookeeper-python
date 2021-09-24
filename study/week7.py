83753743fb118a9f32f8dde766dca7704e28e8f9
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-09-24 17:24:30 +0900

    Create week7.py

---
+++ study/week7.py
@@ -0,0 +1,32 @@
+# 2739
+# n = int(input())
+# print(*[f'{n} * {i} = {n*i}' for i in range(1,10)],sep='\n')
+
+# 2438
+# n = int(input())
+# print(*['*'*i for i in range(1,n+1)],sep='\n')
+
+# 2741
+# n = int(input())
+# print(*[i for i in range(1,n+1)],sep='\n')
+
+# 2439
+# n = int(input())
+# print(*[' '*(n-i)+'*'*i for i in range(1,n+1)],sep='\n')
+
+# 2742
+# n = int(input())
+# print(*[n-i for i in range(1,n+1)],sep='\n')
+
+# 10871
+# n,x = map(int,input().split())
+# lst = list(map(int,input().split()))
+# print(*[i for i in lst if i<x])
+
+# 10950
+# n = int(input())
+# print(*[sum(list(map(int,input().split()))) for _ in range(n)],sep='\n')
+
+# 2884
+# h,m = map(int,input().split())
+# print((h+(m-45)//60)%24,(m-45)%60)
