c4b517c42aaaaf9c1995f0060f69629561f6c77a
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-09-26 00:07:49 +0900

    보물섬
    bfs 알고리즘을 활용한 최대거리 측정문제에 완전탐색알고리즘을 추가한 문제이다.
    생각하기로 루프형태를 생각했지만 실제로 루프가 아닌 형태에서도 최대거리가 나올 수 있으므로 이동방향이 2갈래이하인 모든 지점에 대해 탐색해야 한다.

--- 코딩테스트/Gold5/2589.py
+++ 코딩테스트/Gold5/2589.py
@@ -1,9 +1,85 @@
+# from collections import deque 
+
+# N,M = map(int,input().split())
+# lst = [list(input()) for _ in range(N)]
+# dr,dc = [1,-1,0,0],[0,0,1,-1]
+# answer = 1
+
+# def count1 (q):
+#   cnt = -1
+#   visited = [[False for _ in range(M)] for _ in range(N)]
+#   r,c = q[0]
+#   visited[r][c] = True
+
+#   while q:
+#     cnt += 1
+#     for _ in range(len(q)):
+#       r,c = q.popleft()
+#       for i in range(4):
+#         nr,nc = r+dr[i],c+dc[i]
+#         if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L' and not visited[nr][nc]:
+#           q.append([nr,nc])
+#           visited[nr][nc] = True
+          
+#   return cnt
+
+# def check1():
+#   global answer
+#   for r in range(N):
+#     for c in range(M):
+#       if lst[r][c] == 'L':
+#         num = 0
+#         for i in range(4):
+#           nr,nc = r+dr[i],c+dc[i]
+#           if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L':
+#             num += 1
+#         if num == 1:
+#           cnt = count1(deque([[r,c]]))
+#           answer = max(answer,cnt)
+
+# def count2 (q,visited):
+#   cnt = -1
+#   r,c = q[0]
+#   visited[r][c] = True
+
+#   while q:
+#     cnt += 1
+#     for _ in range(len(q)):
+#       r,c = q.popleft()
+#       for i in range(4):
+#         nr,nc = r+dr[i],c+dc[i]
+#         if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L' and not visited[nr][nc]:
+#           q.append([nr,nc])
+#           visited[nr][nc] = True
+
+#   return [cnt, visited]
+
+# def check2 ():
+#   global answer
+#   visited = [[False for _ in range(M)] for _ in range(N)]
+#   for r in range(N):
+#     for c in range(M):
+#       if lst[r][c] == 'L' and not visited[r][c]:
+#         num = 0
+#         for i in range(4):
+#           nr,nc = r+dr[i],c+dc[i]
+#           if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L':
+#             num += 1
+#         if num == 2:
+#           cnt,visited = count2(deque([[r,c]]),visited)
+#           answer = max(answer,cnt)
+
+# check1()
+# check2()
+# print(answer)
+
+################################################ 2인 경우또한 모두 탐색해야 한다.
 from collections import deque 
 
 N,M = map(int,input().split())
 lst = [list(input()) for _ in range(N)]
 dr,dc = [1,-1,0,0],[0,0,1,-1]
-answer = 1
+answer = 0
 
 def count1 (q):
   cnt = -1
@@ -33,42 +109,9 @@ def check1():
           nr,nc = r+dr[i],c+dc[i]
           if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L':
             num += 1
-        if num == 1:
+        if 0<num<=2 :
           cnt = count1(deque([[r,c]]))
           answer = max(answer,cnt)
 
-def count2 (q,visited):
-  cnt = -1
-  r,c = q[0]
-  visited[r][c] = True
-
-  while q:
-    cnt += 1
-    for _ in range(len(q)):
-      r,c = q.popleft()
-      for i in range(4):
-        nr,nc = r+dr[i],c+dc[i]
-        if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L' and not visited[nr][nc]:
-          q.append([nr,nc])
-          visited[nr][nc] = True
-
-  return [cnt, visited]
-
-def check2 ():
-  global answer
-  visited = [[False for _ in range(M)] for _ in range(N)]
-  for r in range(N):
-    for c in range(M):
-      if lst[r][c] == 'L' and not visited[r][c]:
-        num = 0
-        for i in range(4):
-          nr,nc = r+dr[i],c+dc[i]
-          if 0<=nr<N and 0<=nc<M and lst[nr][nc] == 'L':
-            num += 1
-        if num == 2:
-          cnt,visited = count2(deque([[r,c]]),visited)
-          answer = max(answer,cnt)
-
 check1()
-check2()
 print(answer)
