d028a6d5675dba40696175c4258d65e87cb942e0
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-10-14 08:56:58 +0900

    사다리 조작
    반복문에서 이미 탐색한 부분 또한 계속해서 탐색하고 있었기 때문에 오류가 발생했다. 이후에 탐색한 부분은 다시 재탐색하지 않도록 바꾸어주었다.
    또한 문제에서 주어진 조건을 만족하지 않아 시간초과, 정답 오류가 계속 떴기에 그 부분을 수정하였다.

--- 코딩테스트/Gold4/15684.py
+++ 코딩테스트/Gold4/15684.py
@@ -58,3 +58,71 @@
 
 # print(answer if answer != 300 else -1)
 ########################################### 매우 틀린 테스트 코드 어디가 틀렸는지 확실히 모르지만 재귀함수 부분이 틀렸다.
+
+n,m,h = map(int,input().split())
+lst = [[0]*(n-1) for _ in range(h)]
+visited = [[False]*(n-1) for _ in range(h)]
+answer = 300
+
+for _ in range(m):
+  a,b = map(int,input().split())
+  lst[a-1][b-1] = 1
+  if b<n-1:
+    lst[a-1][b] -= 1
+  if 1<b:
+    lst[a-1][b-2] -= 1
+
+
+def answer_check(lst,cnt):
+  global answer
+  if answer < cnt:
+    return True
+
+  temp = list(range(n))
+  for r in range(h):
+    for c in range(n-1):
+      if lst[r][c] == 1:
+        temp[c],temp[c+1] = temp[c+1],temp[c]
+  
+  if temp == list(range(n)):
+    answer = cnt
+    return True
+  else:
+    return False
+
+def find_list(sr,sc,lst,cnt):
+  if cnt > 2:
+    return
+    
+  for c in range(sc+1,n-1):
+    if lst[sr][c] == 0:
+      lst[sr][c] = 1
+      if c<n-2: lst[sr][c+1] -= 1
+      if 0<c: lst[sr][c-1] -= 1
+
+      if not answer_check(lst,cnt+1):
+        find_list(sr,c,lst,cnt+1)
+
+      lst[sr][c] = 0
+      if c<n-2: lst[sr][c+1] += 1
+      if 0<c: lst[sr][c-1] += 1
+
+
+  for r in range(sr+1,h):
+    for c in range(n-1):
+      if lst[r][c] == 0:
+        lst[r][c] = 1
+        if c<n-2: lst[r][c+1] -= 1
+        if 0<c: lst[r][c-1] -= 1
+
+        if not answer_check(lst,cnt+1):
+          find_list(r,c,lst,cnt+1)
+
+        lst[r][c] = 0
+        if c<n-2: lst[r][c+1] += 1
+        if 0<c: lst[r][c-1] += 1
+
+answer_check(lst,0)
+find_list(0,-1,lst,0)
+
+print(answer if answer != 300 else -1)
