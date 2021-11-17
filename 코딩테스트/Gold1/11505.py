584e337b24d1efbb664bdda832ade8c1cb70dfb6
zookeeper464 <78637227+zookeeper464@users.noreply.github.com>
2021-11-17 10:37:39 +0900

    구간 곱 구하기
    세그먼트 트리의 방식은 여러가지가 있지만 이 방식이 가장 정확하다. 앞으로 여러 변칙적인 방식 속에서 이 방법을 기억하자.

--- 코딩테스트/Gold1/11505.py
+++ 코딩테스트/Gold1/11505.py
@@ -59,3 +59,55 @@ for _ in range(m+k):
     answer.append(check(1,0,n-1,a,b))
 
 print(answer)
+####################################################################### 0을 확인해야 하는 문제가 발생한다. 수정하는 부분에서부터 위로 올라가면서 수정해야 한다.
+def make (node,s,e):
+  if s > e: dp[node] = 1; return 1
+  elif s == e: dp[node] =  lst[s]; return lst[s]
+
+  mid = (s+e)//2
+  temp = (make(node*2,s,mid)*make(node*2+1,mid+1,e))%p
+  dp[node] = temp; return temp
+
+def change(pos, val, node, x, y):
+  if y < pos or pos < x:
+    return dp[node]
+
+  if x == y:
+    dp[node] = val
+    return dp[node]
+
+  mid = (x + y)//2
+  dp[node] = (change(pos,val,node*2,x,mid) * change(pos,val,node*2+1,mid+1,y))%p
+  
+  return dp[node]
+
+def check (node,l,r,s,e):
+  if r<s or l>e:
+    return 1
+
+  elif s<=l and r<=e:
+    return dp[node]
+  
+  mid = (l+r)//2
+  temp = (check(node*2,l,mid,s,e)*check(node*2+1,mid+1,r,s,e))%p
+  return temp
+
+n,m,k = map(int,input().split())
+lst = [int(input()) for _ in range(n)]
+dp = [0] * (4*n)
+answer = []
+p = 1000000007
+
+
+make(1,0,n-1)
+print(dp)
+for _ in range(m+k):
+  x,a,b = map(int,input().split())
+  a -= 1
+  if x == 1:
+    change(a,b,1,0,n-1)
+    print(dp)
+  else:
+    answer.append(check(1,0,n-1,a,b-1))
+    
+print(*answer,sep='\n')
