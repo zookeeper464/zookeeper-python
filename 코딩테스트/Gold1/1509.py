be64961325c830ec07428154774d8efe4c0e17ec
zookeeper464 <rlagkwls1024@gmail.com>
2021-11-26 20:43:56 +0900

    팰린드롬 분할

--- 코딩테스트/Gold1/1509.py
+++ 코딩테스트/Gold1/1509.py
@@ -1,39 +1,36 @@
+import sys
+sys.setrecursionlimit(10**6)
+inf = sys.maxsize
+
 s = input()
 n = len(s)
-dic = {i:[] for i in range(n)}
+mat = [[] for _ in range(n)]
 
 def check(start,end):
-    if start == 0 or end == n-1:
+    if start < 0 or end > n-1:
         return
     
-    if s[start-1] == s[end+1]:
-        dic[start-1].append(end+1)
+    if s[start] == s[end]:
+        mat[end].append(start)
         check(start-1,end+1)
 
-def dfs(idx,cnt,lst):
-    global answer
-    for k in range(idx,n):
-        if dic[k] == []:
-            continue
+def nums(idx):
+    global dp
+    if idx == -1:
+        return 1
 
-        for i in dic[k]:
-            lst.append([k,i])
-            dfs(i+1,cnt-(i-k),lst)
-            lst.pop()
+    if dp[idx] != inf:
+        return dp[idx]+1
+    
+    for i in mat[idx]:
+        dp[idx] = min(dp[idx],nums(i-1))
 
-    if cnt < answer:
-        answer = cnt
+    return dp[idx]+1
 
-for i in range(n-1):
-    if s[i] == s[i+1]:
-        dic[i].append(i+1)
-        check(i,i+1)
+for i in range(n):
+    check(i,i+1)
+    check(i,i)
 
-for i in range(n-2):
-    if s[i] == s[i+2]:
-        dic[i].append(i+2)
-        check(i,i+2)
-
-answer = 2500
-dfs(0,n,[])
-print(answer)
+dp = [inf]*n
+nums(n-1)
+print(dp[-1])
