408a7b400ac115260710dddc1688f45a1570be60
zookeeper464 <rlagkwls1024@gmail.com>
2021-12-01 16:13:59 +0900

    치킨 배달

--- 코딩테스트/Gold5/15686.py
+++ 코딩테스트/Gold5/15686.py
@@ -1,36 +1,45 @@
+def dfs(idx,cnt,lst):
+    global m 
+    if cnt == m:
+        check(lst)
+        return
+
+    for i in range(idx+1,c):
+        lst.append(i)
+        dfs(i,cnt+1,lst)
+        lst.pop()
+
+def check(lst):
+    global answer
+    temp = 0
+    for i in range(h):
+        temp += min(dp[j][i] for j in lst)
+
+    if answer > temp:
+        answer = temp
+
+
 n,m = map(int,input().split())
-lst = [list(map(int,input().split())) for _ in range(n)]
-home,chichi = [], []
-answer = 1000000
+mat = [list(map(int,input().split())) for _ in range(n)]
+chicken,home = [],[]
+
 for r in range(n):
-  for c in range(n):
-    if lst[r][c] == 1:
-      home.append((r,c))
-    elif lst[r][c] == 2:
-      chichi.append((r,c))
-h,ch = len(home),len(chichi)
+    for c in range(n):
+        if mat[r][c] == 1:
+            home.append([r,c])
+        elif mat[r][c] == 2:
+            chicken.append([r,c])
+c,h = len(chicken),len(home)
+dp = [[] for _ in range(c)]
 
-def check(temp):
-  global answer
-  cnt = 0
-  for y,x in home:
-    num = 100
-    for i in temp:
-      r,c = chichi[i]
-      num = min(num, abs(y-r)+abs(x-c))
-    cnt += num
+for i in range(c):
+    a,b = chicken[i]
+    for j in range(h):
+        x,y = home[j]
+        dp[i].append(abs(a-x)+abs(b-y))
 
-  answer = min(answer,cnt)
+answer = 4*(n**2)
+for i in range(c):
+    dfs(i,1,[i])
 
-def dfs (cnt,idx,temp):
-  if cnt == m:
-    check(temp)
-    return
-
-  for i in range(idx+1,ch):
-    temp.append(i)
-    dfs(cnt+1,i,temp)
-    temp.pop()
-
-dfs(0,-1,[])
-print(answer) 
+print(answer)
