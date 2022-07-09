745e7e8b90343d8029365a86bc00ef9fddaaf995
김하진 <78637227+zookeeper464@users.noreply.github.com>
2022-07-09 14:46:33 +0900

    N과 M (8)

---
+++ silver3_15657.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+def input():
+  return stdin.readline().rstrip('\n')
+
+N,M = [int(x) for x in input().split(" ")]
+input_list = input().split(" ")
+input_list.sort(key=lambda x : int(x))
+answer_list = []
+temp_list = []
+
+def ranked_comb(num,idx):
+  global answer_list, input_list, temp_list, M
+  
+  if (num == M):
+    answer_list.append(" ".join(temp_list))
+    return 
+  
+  for i in range(idx,len(input_list)):
+    temp_list.append(input_list[i])
+    ranked_comb(num+1,i)
+    temp_list.pop()
+                  
+ranked_comb(0,0)
+
+print(*answer_list,sep='\n')
