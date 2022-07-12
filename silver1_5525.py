277b5255eeb4e5425830826c468e053c9e12b919
김하진 <78637227+zookeeper464@users.noreply.github.com>
2022-07-12 23:11:08 +0900

    IOIOI

---
+++ silver1_5525.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+def input():
+  return stdin.readline().rstrip('\n')
+
+def KMP(string,pattern):
+  rank,i = [0]*len(pattern),0
+  
+  for j in range(1,len(pattern)):
+    while (i>0 and pattern[i] != pattern[j]):
+      i = rank[i-1]
+      
+    if (pattern[i] == pattern[j]):
+      i += 1
+      rank[j] = i
+    
+  result = []
+  i = 0
+  for j in range(len(string)):
+    while (i>0 and pattern[i] != string[j]):
+      i = rank[i-1]
+      
+    if (pattern[i] == string[j]):
+      i += 1
+      if i == len(pattern):
+        result.append(j-i+1)
+        i = rank[i-1]
+        
+  return result
+N,M = int(input()),int(input())
+string = input()
+pattern = "IO"*N+"I"
+print(len(KMP(string,pattern)))
